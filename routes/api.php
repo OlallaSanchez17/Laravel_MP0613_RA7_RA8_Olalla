f1021ba936eadba3ad623cf468a29fb6a71a2faa
Olalla <olallasanchezbernad08@gmail.com>
2026-03-13 14:53:51 +0100

    api para postman

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Film;
+use App\Models\Actor;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/films', function () {
+    return Film::all();
+});
+
+Route::get('/actors', function () {
+    return Actor::all();
 });
